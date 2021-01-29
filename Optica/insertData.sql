82279f59aae5313288c1d94cc02f78ba1059c770
gambo94 <fedigambo94@gmail.com>
2021-01-29 11:18:32 +0100

    Uploading script and data to be inserted

---
+++ Optica/insertData.sql
@@ -0,0 +1,26 @@
+/* Empleado */
+INSERT INTO empleado VALUES (1, 'Juan Carlos Gómez');
+INSERT INTO empleado VALUES (2, 'Julia Fernández');
+INSERT INTO empleado VALUES (3, 'Ariadna Sánchez');
+
+/* Proveedor */
+INSERT INTO proveedor VALUES ('M02000049H', 'Juanito Carlito', 'Calle de los Peatones 40, 4 2', 08034, 'Barcelona', 'España', '632443564', NULL);
+INSERT INTO proveedor VALUES ('F03495434H', 'Valerita Valeriata', 'Calle Arcoiris 54, 3 2', 09745, 'Pamplona', 'España', '634736234', NULL);
+INSERT INTO proveedor VALUES ('G345435G', 'Pedro Patri', 'Calle Palomas 32 4 3', 05432, 'Almería', 'España', '621345367', NULL);
+
+/* Cliente */
+INSERT INTO cliente VALUES (1, 'Marino Colapescado', 'Calle de los Peces 32, 3 1', 'marinitopez@hola.es', CURRENT_TIMESTAMP(), NULL);
+INSERT INTO cliente VALUES (2, 'Jennifer López', 'Calle from the Block 12, 2 2', 'jennyftb@jolly.pt', CURRENT_TIMESTAMP(), 'Marino Colapescado');
+INSERT INTO cliente VALUES (3, 'Rachel Ohio', 'Calle de Lima 21, 4 3', 'rachelb@gll.us', CURRENT_TIMESTAMP(), 'Jennifer López');
+
+/* Marca */
+INSERT INTO marca VALUES ('Rayban', 'M02000049H');
+INSERT INTO marca VALUES ('Carrera', 'M02000049H');
+INSERT INTO marca VALUES ('MochiMochi', 'F03495434H');
+
+/* Gafas */ 
+INSERT INTO gafas VALUES (100, 1.2, 1, 'chula', 'amarillo', 'marrón', 34, 'Rayban', NULL, NULL);
+INSERT INTO gafas VALUES (101, 2.3, 2.2, 'chulísima', 'rojo', 'rojo', 67, 'Carrera', 1, 2);
+INSERT INTO gafas VALUES (102, 3.2, 1.2, 'fea', 'negro', 'blanco', 90, 'MochiMochi', 2, 3);
+
+
--- Optica/script.sql
+++ Optica/script.sql
@@ -4,13 +4,14 @@ USE optica;
 
 CREATE TABLE empleado (
     id_empleado INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    nombre VARCHAR(100) NOT NULL
+    nombre_empleado VARCHAR(100) NOT NULL
 );
 
 CREATE TABLE proveedor (
     nif VARCHAR(20) PRIMARY KEY,
+    nombre_proveedor VARCHAR(20) NOT NULL,
     adr VARCHAR(50) NOT NULL,
-    cp VARCHAR(8) NOT NULL,
+    cp INT NOT NULL,
     ciudad VARCHAR(15) NOT NULL,
     pais VARCHAR(20) NOT NULL,
     telefono VARCHAR(9) NOT NULL,
@@ -38,12 +39,12 @@ CREATE TABLE gafas (
     graduación_derecha FLOAT NOT NULL,
     graduación_izquierda FLOAT NOT NULL,
     tipo_montatura VARCHAR(10) NOT NULL,
-    coloR_montatura VARCHAR(10) NOT NULL,
+    color_montatura VARCHAR(10) NOT NULL,
     color_vidrio VARCHAR(19) NOT NULL,
     precio FLOAT NOT NULL,
     marca VARCHAR(15) NOT NULL,
-    id_empleado INT UNSIGNED NOT NULL,
-    id_cliente INT UNSIGNED NOT NULL,
+    id_empleado INT UNSIGNED,
+    id_cliente INT UNSIGNED,
     FOREIGN KEY (marca) REFERENCES marca(marca),
     FOREIGN KEY (id_empleado) REFERENCES empleado(id_empleado),
     FOREIGN KEY (id_cliente) REFERENCES cliente(id_cliente)
